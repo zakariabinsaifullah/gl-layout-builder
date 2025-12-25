bd884dd2e0a20f032f7f5318bcb27264bc841d3b
Zakaria Binsaifullah <zbinsaifullah@gmail.com>
2025-12-25 15:53:40 +0600

    Delete build directory

--- build/blocks/rating/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '28aeb6f74f2b20cad261');
