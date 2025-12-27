334e03f26fdfc5248ef23e331e9d69e253acde8c
Zakaria Binsaifullah <zbinsaifullah@gmail.com>
2025-12-27 13:17:46 +0600

    Delete build directory

--- build/blocks/slider/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '1701b5577ea9bd4f1df3');
--- build/blocks/slider/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '5e918d8e351fc4f47350');
